edc8ac451b3720e0c6165b2d0953fa53dd898ee4
Florian Gardy <florian.gardy@gmail.com>
2019-03-18 17:10:10 +0100

    creation florianGardy.js

---
+++ florianGardy.js
@@ -0,0 +1,11 @@
+// Atelier JS ES6
+
+const hello = (firstName) => `Hello ${firstName}, welcome into the wild`;
+
+console.log(hello("Coco l'asticot"));
+
+const fruits = ["bananas", "oranges", "strawberries"];
+const vegetables = ["carrots", "tomatoes"];
+const fruitsAndVegetables = [...fruits, ...vegetables];
+
+console.log (fruitsAndVegetables);
