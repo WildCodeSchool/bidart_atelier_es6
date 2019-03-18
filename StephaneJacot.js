174619c7d261127e9cedc3df7c7e6e239bcbfa54
Stephane Jacot <stephanejacot@icloud.com>
2019-03-18 17:20:28 +0100

    Atelier made

---
+++ StephaneJacot.js
@@ -0,0 +1,9 @@
+const hello = (firstName) => `Hello ${firstName}, welcome into the wild`;
+
+console.log(hello('Steph'));
+
+const fruits = ["bananas", "oranges", "strawberries"];
+const vegetables = ["carrots", "tomatoes"];
+const fruitsAndVegetables = [...fruits, ...vegetables];
+
+console.log (fruitsAndVegetables);
