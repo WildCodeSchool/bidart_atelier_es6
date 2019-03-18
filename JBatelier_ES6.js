54aff3fc2887b1b0f930343e7defb087b862d4a8
Jean-Baptiste <gervais.jbaptiste@gmail.com>
2019-03-18 17:52:48 +0100

    jb

---
+++ JBatelier_ES6.js
@@ -0,0 +1,7 @@
+const hello = (firstName) => `Hello ${firstName} welcome into the wild`;
+
+
+const fruits = ['ananas', 'abricot', 'coco'];
+const vegetables = ['carrot', 'louis'];
+const result = [...fruits, ...vegetables];
+console.log(result)
